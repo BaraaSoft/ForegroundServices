e8f1ae242d5706d45a2e18c08857585bbe2a4880
baraa <baraaabuzeid@hotmail.com>
2018-10-17 02:45:49 +0300

    simply location tracking

---
+++ app/src/main/java/com/baraa/software/eventhorizon/trackingapp/LocationService.kt
@@ -0,0 +1,173 @@
+package com.baraa.software.eventhorizon.trackingapp
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationChannel.DEFAULT_CHANNEL_ID
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.location.Location
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.content.LocalBroadcastManager
+import android.util.Log
+import com.google.android.gms.location.*
+
+class LocationService:Service() {
+
+    companion object {
+        const val ACTION_START = "LocationService.start"
+        const val ACTION_STOP = "LocationService.stop"
+        const val LOCATION_RECEIVER = "com.trackingapp.locationReceiver"
+
+        const val LOCATION_NOTIFICATON_CHANNEL = "location_notification_channel"
+    }
+
+    val REQUEST_CHECK_SETTINGS:Int = 1234
+    private val UPDATE_INTERVAL = (10 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
+
+    lateinit var mLocationRequest: LocationRequest
+    var serviceId:Int? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var action = intent?.action
+        when(action){
+            ACTION_START -> {
+                stopService(serviceId)
+                serviceId = startId
+                startLocationUpdates()
+                buildNotification()
+            }
+            ACTION_STOP ->{
+                stopService(serviceId)
+            }
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun stopService(startId: Int?){
+        startId?.let { stopSelf(it) }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    protected fun startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = LocationRequest().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = UPDATE_INTERVAL
+            fastestInterval = FASTEST_INTERVAL
+        }
+
+        // Create LocationSettingsRequest object using location request
+        val builder = LocationSettingsRequest.Builder().apply {
+            addLocationRequest(mLocationRequest)
+        }.build()
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        val settingsClient = LocationServices.getSettingsClient(this)
+
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                // do work here
+                onLocationChanged(locationResult?.lastLocation)
+            }
+        }, Looper.myLooper())
+
+    }
+
+
+    private fun onLocationChanged(lastLocation: Location?) {
+        Log.e("LocationService","ID ::$serviceId >>${lastLocation?.latitude}, ${lastLocation?.longitude}")
+        LocalBroadcastManager.getInstance(this)
+                .also {
+                    Intent(LOCATION_RECEIVER).apply {
+                        putExtra("lat",lastLocation?.latitude)
+                        putExtra("lng",lastLocation?.longitude)
+                    }.also {intent ->
+                        it.sendBroadcast(intent)
+                    }
+                }
+        val textContent = "Your current location is ${lastLocation?.latitude}, ${lastLocation?.longitude}"
+        updateNotification(serviceId ?:0,textContent)
+    }
+
+
+    private fun dissmissNotification(notificationId:Int){
+        with(NotificationManagerCompat.from(this)) {
+            // dismiss notification based on it's Id
+            serviceId?.run {
+                cancel(this)
+            }
+
+        }
+    }
+
+
+    private fun updateNotification(notificationId:Int,txt:String){
+        val notification =  NotificationCompat.Builder(this, LOCATION_NOTIFICATON_CHANNEL)
+                .setSmallIcon(R.drawable.map_ok)
+                .setOnlyAlertOnce(true).setContentText(txt)
+                .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.map_normal))
+                .build()
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            serviceId?.run {
+                notify(this, notification)
+            }
+
+        }
+    }
+
+
+    private fun buildNotification(){
+        val notification = NotificationCompat.Builder(this, LOCATION_NOTIFICATON_CHANNEL)
+                .setSmallIcon(R.drawable.map_ok)
+                .setContentTitle("Your current location")
+                .setContentText("Check out your current location")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setLargeIcon(BitmapFactory.decodeResource(resources,R.drawable.map_normal))
+                .build()
+        startForeground(serviceId!!, notification)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val descriptionText ="location update notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(DEFAULT_CHANNEL_ID, LOCATION_NOTIFICATON_CHANNEL, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/baraa/software/eventhorizon/trackingapp/MainActivity.kt
@@ -0,0 +1,139 @@
+package com.baraa.software.eventhorizon.trackingapp
+
+import android.Manifest
+import android.content.*
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+
+class MainActivity : AppCompatActivity() {
+
+    private val UPDATE_INTERVAL = (10 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
+    val REQUEST_CHECK_SETTINGS:Int = 11707
+
+    val PERMISSIONS_REQUEST_LOCATION:Int = 10177
+    var isLocationPermissionGranted:Boolean = false
+    lateinit var mLocationRequest: LocationRequest
+
+
+    var locationBroadcastReceiver: BroadcastReceiver = object :BroadcastReceiver(){
+        override fun onReceive(p0: Context?, intent: Intent?) {
+            var lat = intent?.getDoubleExtra("lat",0.0)
+            var lng = intent?.getDoubleExtra("lng",0.0)
+            onLocationUpdate(lat,lng)
+        }
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(!checkLocationPermissions())requestLocationPermission()
+        else isLocationPermissionGranted = true
+
+
+        Intent(this, LocationService::class.java).also { intent ->
+            intent.action= LocationService.ACTION_START
+            startService(intent)
+        }
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(locationBroadcastReceiver, IntentFilter(LocationService.LOCATION_RECEIVER))
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // ask the user for permission to change the location settings if its not available
+        changeLocationSetting()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        LocalBroadcastManager
+                .getInstance(this)
+                .unregisterReceiver(locationBroadcastReceiver)
+    }
+
+
+    fun onLocationUpdate(lat:Double?,lng:Double?){
+        Toast.makeText(this,"your Current location $lat , $lng",Toast.LENGTH_LONG).show()
+    }
+
+
+    protected fun changeLocationSetting() {
+
+        mLocationRequest = LocationRequest().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = UPDATE_INTERVAL
+            fastestInterval = FASTEST_INTERVAL
+        }
+
+
+        // Create LocationSettingsRequest object using location request
+        val locationSettingsRequest = LocationSettingsRequest.Builder()
+                .apply { addLocationRequest(mLocationRequest) }
+                .build()
+
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        val settingsClient = LocationServices.getSettingsClient(this)
+        settingsClient.checkLocationSettings(locationSettingsRequest).addOnFailureListener{
+            if (it is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    it.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                isLocationPermissionGranted = (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                return
+            }
+
+        // Add other 'when' lines to check for other
+        // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
+    fun checkLocationPermissions():Boolean{
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+    fun requestLocationPermission(){
+        ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_LOCATION)
+    }
+}
